2aecb9d228989f4b77f62abe2f4ea7b6976a9e42
Jordan Manly <j.manly632@gmail.com>
2023-08-25 23:24:01 -0700

    Add 1-1 to repo

---
+++ Module1_1/src/Module1_1.java
@@ -0,0 +1,7 @@
+public class Module1_1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun!");
+    }
+}
