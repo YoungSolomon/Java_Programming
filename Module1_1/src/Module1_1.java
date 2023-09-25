a646289959edc8fbda79f610eaa32e7b88549ab1
Jordan Manly <j.manly632@gmail.com>
2023-09-24 21:23:01 -0400

    Delete all redundant directories

--- Module1_1/src/Module1_1.java
+++
@@ -1,7 +0,0 @@
-public class Module1_1 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to Java");
-        System.out.println("Welcome to Computer Science");
-        System.out.println("Programming is fun!");
-    }
-}
---
+++ Test/src/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[] args) throws Exception {
+        int x = 0;
+        if(x< 4){
+            x = x +1;
+
+        }
+            System.out.println(x);
+        }
+
+
+        
+        }
+    
+
